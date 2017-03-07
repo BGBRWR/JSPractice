d8b0698387eb14c985d855cb802189857a843256
Austin Walker <bgbrwr@gmail.com>
2017-03-07 10:38:44 -0700

    array practice

--- scripts.js
+++ scripts.js
@@ -6,15 +6,23 @@ animal['tagline'] = "that's me"
 
 var noises = []
 animal.noises = noises
-console.log(animal)
 
-var count = 0
+// var count = 0
 for (var key in animal) {
-  count++
+  // count++
   if (key === 'username') {
     console.log('Hi my name is ' + animal[key])
   } else if (key === 'tagline') {
     console.log('I like to say ' + animal[key])
   }
-  console.log(count)
+  // console.log(count)
 }
+
+var noiseArray = ['Bwaaa']
+noiseArray.unshift('Moo')
+noiseArray.push('RRRrr')
+noiseArray[3] = 'woof'
+console.log(noiseArray)
+
+animal.noises = noiseArray
+console.log(animal)
